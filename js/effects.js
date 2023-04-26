761cb6520bd9a2818b7706dba8e8a73103b2a961
Samuel Balcells <sfaith3@users.noreply.github.com>
2023-04-26 19:18:11 +0200

    Some organization in styles.css and metas + Add some meta in index.html + Create effects.js

---
+++ js/effects.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    // fadeInElements = document.getElementsById("fadeIn");
+
+    // var myScrollFunc = function () {
+    //     var y = window.scrollY;
+    //     if (y >= 800) {
+    //         myID.className = "cta show"
+    //     } else {
+    //         myID.className = "cta hide"
+    //     }
+    // };
+
+    // window.addEventListener("scroll", myScrollFunc);
+
+});
