71932037c10ba2d4aa2ba39d36f02f17d2e66e7a
Samuel Balcells <sfaith3@users.noreply.github.com>
2023-04-26 23:38:20 +0200

    Add an effect for experience and education section

--- js/effects.js
+++ js/effects.js
@@ -1,16 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
-
-    // fadeInElements = document.getElementsById("fadeIn");
-
-    // var myScrollFunc = function () {
-    //     var y = window.scrollY;
-    //     if (y >= 800) {
-    //         myID.className = "cta show"
-    //     } else {
-    //         myID.className = "cta hide"
-    //     }
-    // };
-
-    // window.addEventListener("scroll", myScrollFunc);
-
+    InitHideElements();
 });
+
+function InitHideElements() {
+    const experienceRowElements = document.getElementsByClassName("experience-row");
+    const educationRowElements = document.getElementsByClassName("education-row");
+
+    function ExperienceRow(event) {
+        AreVisibleInScreen(ExperienceRow, experienceRowElements, "hide-left");
+    }
+    function EducationRow(event) {
+        AreVisibleInScreen(EducationRow, educationRowElements, "hide-left");
+    }
+
+    window.addEventListener("scroll", ExperienceRow);
+    window.addEventListener("scroll", EducationRow);
+}
+
+function AreVisibleInScreen(functionListened, elements, classToRemove) {
+    let arePendingClassesToRemove = false;
+    const OffsetY = 600; // To get 'value' earlier
+
+    Array.prototype.forEach.call(elements, element => {
+        if (!element.classList.contains(classToRemove)) { return; }
+
+        arePendingClassesToRemove = true;
+
+        const hideAnimRect = element.getBoundingClientRect();
+        if (window.scrollY >= (hideAnimRect.top - OffsetY) + window.scrollY) {
+            element.classList.remove(classToRemove);
+        }
+    });
+
+    if (!arePendingClassesToRemove) {
+        window.removeEventListener("scroll", functionListened);
+    }
+};
