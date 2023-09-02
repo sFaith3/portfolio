d59f44f79230bddcd5365af7551bc26319b2c3f7
Samuel Balcells <sFaith3@users.noreply.github.com>
2023-09-02 15:08:55 +0200

    Update description meta, intro, contact and bottom sections

--- js/effects.js
+++ js/effects.js
@@ -1,38 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
-    InitHideElements();
+  InitHideElements();
 });
 
 function InitHideElements() {
-    const experienceRowElements = document.getElementsByClassName("experience-row");
-    const educationRowElements = document.getElementsByClassName("education-row");
+  const experienceRowElements =
+    document.getElementsByClassName("experience-row");
+  const educationRowElements = document.getElementsByClassName("education-row");
 
-    function ExperienceRow(event) {
-        AreVisibleInScreen(ExperienceRow, experienceRowElements, "hide-left");
-    }
-    function EducationRow(event) {
-        AreVisibleInScreen(EducationRow, educationRowElements, "hide-left");
-    }
+  function ExperienceRow(event) {
+    AreVisibleInScreen(ExperienceRow, experienceRowElements, "hide-left");
+  }
+  function EducationRow(event) {
+    AreVisibleInScreen(EducationRow, educationRowElements, "hide-left");
+  }
 
-    window.addEventListener("scroll", ExperienceRow);
-    window.addEventListener("scroll", EducationRow);
+  window.addEventListener("scroll", ExperienceRow);
+  window.addEventListener("scroll", EducationRow);
 }
 
 function AreVisibleInScreen(functionListened, elements, classToRemove) {
-    let arePendingClassesToRemove = false;
-    const OffsetY = 1000; // To get 'value' earlier
+  let arePendingClassesToRemove = false;
+  const OffsetY = 1000; // To get 'value' earlier
 
-    Array.prototype.forEach.call(elements, element => {
-        if (!element.classList.contains(classToRemove)) { return; }
-
-        arePendingClassesToRemove = true;
-
-        const hideAnimRect = element.getBoundingClientRect();
-        if (window.scrollY >= (hideAnimRect.top - OffsetY) + window.scrollY) {
-            element.classList.remove(classToRemove);
-        }
-    });
-
-    if (!arePendingClassesToRemove) {
-        window.removeEventListener("scroll", functionListened);
+  Array.prototype.forEach.call(elements, (element) => {
+    if (!element.classList.contains(classToRemove)) {
+      return;
     }
-};
+
+    arePendingClassesToRemove = true;
+
+    const hideAnimRect = element.getBoundingClientRect();
+    if (window.scrollY >= hideAnimRect.top - OffsetY + window.scrollY) {
+      element.classList.remove(classToRemove);
+    }
+  });
+
+  if (!arePendingClassesToRemove) {
+    window.removeEventListener("scroll", functionListened);
+  }
+}
